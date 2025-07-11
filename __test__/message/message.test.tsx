169372288bbbb47901dd55d0817d9bc23ef3d472
André Luiz <154485939+andreluizdasilvaa@users.noreply.github.com>
2025-07-11 15:43:55 -0300

    Teste com Timers - jest CORRIGIDO

--- __test__/message/message.test.tsx
+++ __test__/message/message.test.tsx
@@ -2,6 +2,13 @@ import { render, screen, fireEvent, act } from "@testing-library/react-native";
 import Message from "@/src/Message";
 
 describe("Component Message", () => {
+
+    afterAll(() => {
+        // Remove quaisquer temporizadores pendentes do sistema de temporizadores.
+        // Isso significa que, se algum temporizador tiver sido agendado (mas ainda não tiver sido executado), ele será limpo e nunca mais terá a oportunidade de ser executado no futuro.
+        jest.clearAllTimers();
+    })
+
     it("should render text message", () => {
         const { getByTestId } = render(<Message />)
 
@@ -14,12 +21,6 @@ describe("Component Message", () => {
 
     it("should change message on click button", async () => {
 
-        afterAll(() => {
-            // Remove quaisquer temporizadores pendentes do sistema de temporizadores.
-            // Isso significa que, se algum temporizador tiver sido agendado (mas ainda não tiver sido executado), ele será limpo e nunca mais terá a oportunidade de ser executado no futuro.
-            jest.clearAllTimers();
-        })
-
         // Como nosso teste tem o setTimeOut ( demorando 2s para o efeito clicando no btn aparecer ), usamos o jest.useFakeTimers(); que ele "avança no tempo" e não precisando esperar os 2s do setTimeOut no teste
         // https://jestjs.io/docs/timer-mocks
         jest.useFakeTimers();
@@ -31,9 +32,11 @@ describe("Component Message", () => {
         // o ACT espera todos as re-renderizações, efeitos colaterais etc, estejam concluídas antes de prosseguir, sem isso 
         // É obrigatório envolver com 'act' qualquer chamada que dispara atualizações de estado ou efeitos, no caso clicar no btn e mudar o texto
         // https://github.com/threepointone/react-act-examples/blob/master/sync.md
+
+        // o evento é antes de "jest.runAllTimers();"
         act(() => {
-            jest.runAllTimers();
             fireEvent.press(screen.getByText("Acessar"))
+            jest.runAllTimers();
         });
         
         // Aqui ele pega o elemento com o id de teste e verifica se o children é igual a Bem vindo!
@@ -45,6 +48,6 @@ describe("Component Message", () => {
         // e espera que esse texto seja "Bem vindo!" ( aqui testamos o elemento )
         expect(text.props.children).toBe("Bem vindo!")
 
-    })
+    });
 
 })
